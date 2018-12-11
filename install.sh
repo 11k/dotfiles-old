effd00ce9a0a853deda2b712434a171af451ad8c
Farhad Jay <effejaye@gmail.com>
2018-12-10 17:35:42 -0800

    Allow installing dotfiles from any directory

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-DOTFILES_DIR=${PWD##*/}
+DOTFILES_DIR="$PWD"
 
 echo "Linking dotfiles..."
 
@@ -11,7 +11,7 @@ do
         continue
     fi
 
-    ln -fsv ~/$DOTFILES_DIR/$DOTFILE ~
+    ln -fsv "$DOTFILES_DIR/$DOTFILE" ~
 done
 
 echo "Finished!"
